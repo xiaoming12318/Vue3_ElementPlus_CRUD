fb3009e7fef94ff39fc5f307c754244fb6cdf0b6
Jacob <975409698@qq.com>
2023-06-12 18:28:26 +0800

    删除模块

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -4,16 +4,40 @@ import Edit from './components/edit.vue'
 import {ElMessage} from 'element-plus'
 import axios from 'axios'
 
+//数组数据
 const list=ref([])
+//list列表
 const getList=async()=>{
     const res =await axios.get('/list')
     console.log(res)
     list.value=res.data
 }
 
+//删除数据
+const deleteById=async(data)=>{
+    //获取数据
+    const id = data.row.id
+    //发送axios，删除数据
+    const res=await axios.delete(`/del/${id}`)
+    console.log(res)
+    //更新列表
+    getList()
+}
+
+//编辑数据
+const editRef=ref(null)
+const onEdit=(row)=>{
+    console.log(editRef)
+    editRef.value.openDialog(row.row)
+}
+
+
+//钩子函数
 onMounted(()=>{
     getList()
 })
+
+
 
 </script>
 
@@ -24,13 +48,15 @@ onMounted(()=>{
             <el-table-column prop="name" label="name"></el-table-column>
             <el-table-column prop="place" label="place"></el-table-column>
             <el-table-column prop="operation" label="operation">
-                <template #default>
-                    <el-button type="primary" link >编辑</el-button>
-                    <el-button type="danger" link >删除</el-button>
+                <template #default=row>
+                    <el-button type="primary" link @click="onEdit(row)">编辑</el-button>
+                    <el-button type="danger" link @click="deleteById(row)">删除</el-button>
                 </template>
             </el-table-column>
         </el-table>
     </div>
+
+    <Edit ref="editRef" />
 </template>
 
 
--- vue-project/src/components/edit.vue
+++ vue-project/src/components/edit.vue
@@ -1,11 +1,51 @@
 <script setup>
-import {ref} from 'vue';
+import {ref,reactive} from 'vue';
 
-
+const list =reactive({
+    name:'',
+    place:''
+})
+const dialogVisible=ref(false)
+const openDialog=(row)=>{
+    list.name=row.name
+    list.place=row.place
+    dialogVisible.value=true
+}
+defineExpose({
+    openDialog
+}) 
 
 </script>
 
 
+
 <template>
+    <el-dialog 
+    v-model="dialogVisible"
+    title="编辑"
+    width="400px"
+    >
+        <el-form :model="form" label-width="50px">
+            <el-form-item label="姓名">
+                <el-input v-model="list.name" autocomplete="off"/>
+            </el-form-item>
+            <el-form-item label="地区">
+                <el-input v-model="list.place"/>
+            </el-form-item>
+        </el-form>
+            <template #footer>
+                <span class="dialog-footer">
+                    <el-button  @click="dialogVisible = false">取消</el-button>
+                    <el-button  type="primary" @click="onUpdate">确认</el-button>
+                </span>
+            </template>
+    </el-dialog>
+    
 
 </template>
+
+<style scoped>
+.el-input {
+  width: 290px;
+}
+</style>
